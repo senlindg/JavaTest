dab6d155b9ebef3b4e6e4ab2b260969cb732c587
GenYL <6331177@163.com>
2014-09-19 11:11:05 +0800

    项目提交

---
+++ EJBTestJava/src/com/demo/HelloWorld1.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.demo;
+
+/**
+ * @author Administrator
+ *
+ */
+public class HelloWorld1
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
